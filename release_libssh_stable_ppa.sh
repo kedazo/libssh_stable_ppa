bf9106a8a7a37098a25887044abcb7b25d33daf8
David Kedves <david.kedves@borgcollective.eu>
2019-04-30 10:33:44 +0200

    Adding PPA releaser script

---
+++ release_libssh_stable_ppa.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# David Kedves <kedazo@gmail.com>
+
+if [ ! -d debian ]; then
+    echo "Please execute this script inside the project directory."
+    exit 1
+fi
+
+echo "WARNING: this script will remove all ../libssh* files"
+read
+
+rm -fv ../libssh*
+
+for distro in trusty xenial bionic cosmic disco; do
+    debchange --team --distribution=$distro "Release for $distro."
+
+    dpkg-buildpackage -rfakeroot -S
+done
+
+dput ppa:kedazo/libssh-0.7.x ../libssh*.changes
+
