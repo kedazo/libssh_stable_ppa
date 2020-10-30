155fe4e2e34b9a02d0b03139cd02d32367264b68
David Kedves <kedz@kedz.eu>
2020-10-30 08:07:17 +0100

    Import from https://www.libssh.org/files/0.9/libssh-0.9.5.tar.xz

--- release_libssh_stable_ppa.sh
+++ release_libssh_stable_ppa.sh
@@ -12,7 +12,7 @@ read
 rm -fv ../libssh*
 
 # trusty is too old
-for distro in xenial bionic eoan; do
+for distro in xenial bionic eoan focal; do
     debchange --team --distribution=$distro "Release for $distro."
 
     dpkg-buildpackage -rfakeroot -S
